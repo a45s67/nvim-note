534dcc2ef82a0bf1555b240fd2b50f0b13545470
Fish <b45s67@gmail.com>
2023-01-07 23:02:17 +0800

    chore: Disable coc when using nvim-lspconfig.

--- plugin/nvim-lspconfig.lua
+++ plugin/nvim-lspconfig.lua
@@ -9,6 +9,8 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  -- Disable coc to prevent collision
+  vim.api.nvim_command('CocDisable')
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
