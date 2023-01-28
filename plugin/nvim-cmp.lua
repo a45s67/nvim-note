4398486cfb00c8962672aab2aeaf3597f4702a93
xiang yu, liu <a45s67@chtsecurity.com>
2023-01-28 14:49:43 +0800

    enhance: Add nvimlsp support for go

--- plugin/nvim-cmp.lua
+++ plugin/nvim-cmp.lua
@@ -4,7 +4,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+local servers = { 'clangd', 'gopls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
@@ -46,5 +46,6 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'ultisnips' },
+    { name = 'path' },
   },
 }
