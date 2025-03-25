56e891d41d393a498cca20ccc502826a3b25e905
Fish Yu <b45s67@gmail.com>
2025-03-25 17:57:50 +0800

    feat: Add which key

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,24 @@ require("lazy").setup({
         'honza/vim-snippets',
 
         { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+        {
+            "folke/which-key.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            },
+            keys = {
+                {
+                    "<leader>?",
+                    function()
+                        require("which-key").show({ global = false })
+                    end,
+                    desc = "Buffer Local Keymaps (which-key)",
+                },
+            },
+        },
 
         'phaazon/hop.nvim',
         {
